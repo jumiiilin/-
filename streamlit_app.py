626bffa9ec0fb9a28f4bf3a6715c66d3615e7fc9
30305 <dessertyy18@naver.com>
2025-06-09 12:41:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,11 @@ import streamlit as st
 
 st.title("서울시 상권 vs 유동인구 분석")
 st.write("Streamlit 앱이 성공적으로 실행되었습니다!")
+streamlit
+pandas
+scikit-learn
+folium
+streamlit-folium
 import pandas as pd
 
 def group_subway_timezones(df_subway):
